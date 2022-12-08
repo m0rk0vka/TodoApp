b68def4d425aa88a5c348eaee6f09c750526ea09
t.vladimir <vovatatosyan2000@mail.ru>
2022-12-08 15:51:53 +0300

    Added init postgres

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,27 @@ import (
 	"github.com/m0rk0vka/TodoApp/pkg/handler"
 	"github.com/m0rk0vka/TodoApp/pkg/repository"
 	"github.com/m0rk0vka/TodoApp/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initialization configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
 
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
